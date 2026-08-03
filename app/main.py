8106efe9ddadf8055b230679c967850286ebafd0
krisdamx <fsilvalagos@gmail.com>
2026-08-02 21:14:48 -0600

    chore(cors): allow multiple frontend development origins

--- app/main.py
+++ app/main.py
@@ -13,6 +13,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://localhost:3000",
         "http://localhost:5173",
     ],
     allow_credentials=True,
